48379931422e73c55335b9f237930ee773d868f5
Okafor Irene <okaforirenen@gmail.com>
2019-02-18 03:40:57 +0100

    Worked on meal controller again

--- api/controllers/meal.controller.js
+++ api/controllers/meal.controller.js
@@ -7,28 +7,28 @@ const MealController = {
       count: allMeals.length,
       meals: allMeals,
     };
-    res.status(200).json(response);
+    return res.status(200).json(response);
   },
   addMeal(req, res) {
     const newMeal = req.body;
     const createdMeal = MealService.addMeal(newMeal);
-    res.status(201).json({ createdMeal });
+    return res.status(201).json({ createdMeal });
   },
   deleteMeal(req, res) {
     const mealid = req.params.id;
     MealService.deleteMeal(mealid);
-    res.status(200).json({ message: 'Meal deleted' });
+    return res.status(200).json({ message: 'Meal deleted' });
   },
   modifyMeal(req, res) {
     const mealid = req.params.id;
     const updates = req.body;
     const modMeal = MealService.modifyMeal(mealid, updates);
-    res.status(200).json({ modMeal });
+    return res.status(200).json({ modMeal });
   },
   getMeal(req, res) {
     const mealid = req.params.id;
     const Meal = MealService.getMeal(mealid);
-    res.status(200).json({ Meal });
+    return res.status(200).json({ Meal });
   },
 };
 
