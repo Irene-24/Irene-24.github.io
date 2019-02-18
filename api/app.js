2dad6b20bc30009d0c7d5d9fbb634eb3e8c7fef1
Okafor Irene <okaforirenen@gmail.com>
2019-02-18 03:49:58 +0100

    Worked on meal controller again

--- api/app.js
+++ api/app.js
@@ -1,8 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import MealController from './controllers/meal.controller';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// PREVENT CORS ERRORS
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Aloow-Methods', 'PUT, POST, DELETE, PATCH, GET');
+    res.status(200).json({});
+  }
+
+  next();
+});
+
+app.get('/', MealController.getAllMeal());
 
 app.get('/', (req, res) => {
   res.status(200);
@@ -12,5 +29,4 @@ app.get('/', (req, res) => {
     },
   );
 });
-
 export default app;
