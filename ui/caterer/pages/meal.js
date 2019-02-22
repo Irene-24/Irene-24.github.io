dde3dbfccc2ad5baf03a9e8b5db25f1ddd6bb70a
Okafor Irene <okaforirenen@gmail.com>
2019-02-22 23:44:31 +0100

    worked on modals

--- ui/caterer/pages/meal.js
+++ ui/caterer/pages/meal.js
@@ -3,3 +3,30 @@ const addBtn = document.querySelector(".addBtn");
 const addMealForm = document.querySelector("#addMeal");
 const modMealForm = document.querySelector("#modMeal"); 
 const delMeal = document.querySelector("#delMeal"); 
+
+function addMealHandler()
+{
+
+    let reader = new FileReader();
+    let addMealForm = document.querySelector('#addMeal');
+    addMealForm.style.display = 'block';
+    let output = addMealForm.querySelector('#output_image');
+
+    const addMeal = (event) => 
+    {
+        event.preventDefault();
+        let mealTitle = addMealForm['food-name'].value;
+        let mealPrice = addMealForm['food-price'].value;
+        console.log(mealTitle);
+        console.log(mealPrice);
+        
+    }; 
+
+    addMealForm.addEventListener('submit',addMeal,false);
+
+       
+    
+
+}
+
+addBtn.addEventListener('click',addMealHandler,false)
